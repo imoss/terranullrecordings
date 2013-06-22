223c8ae81d63b741ab57416e3b75cc7736e5b210
imoss <imoss@berklee.edu>
2013-06-22 18:37:53 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Terranullrecordings::Application.config.secret_key_base = '24a30f4bd71515249d1ca749f1b5cdda5cff40b2601b37cf5567a003f70a2692b14998486b39cbb32f015a23c4c50a773bd98b4307b434d885726d73ea0aa1a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Terranullrecordings::Application.config.session_store :cookie_store, key: '_terranullrecordings_session'
