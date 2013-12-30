a007ed7b22a34a3d5c6a26010548b44522fbd025
imoss <imoss@berklee.edu>
2013-12-30 16:24:22 -0500

    temp redirect prior to launch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # before_filter :temporary_redirect
+
+  private
   def welcome
     session[:welcomed] = true
+  end
+
+  def temporary_redirect
+    unless controller_name == 'welcome' && action_name == 'index'
+      redirect_to welcome_index_path
+    end
   end
 end
