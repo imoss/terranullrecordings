f28a737bff685286178f04e4cb1313211bc2859b
imoss <imoss@berklee.edu>
2014-07-14 14:53:20 -0400

    remove unused files

--- app/controllers/welcome_controller.rb
+++
@@ -1,9 +0,0 @@
-class WelcomeController < ApplicationController
-  layout false
-
-  before_filter :welcome
-
-  def index
-    session[:welcomed] = true
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Terranullrecordings::Application.routes.draw do
-  resources :about, only: :index
   resources :news_items, only: [:index, :show], path: '/news'
   resources :podcasts, only: :index
   resources :releases, only: [:index, :show]
-  resources :welcome, only: :index
 
   root :to => 'homepage#index'
 end
